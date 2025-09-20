2c58afcbc8ae1450642832a7467978fbd2082dc4
afnx <afndeveloper@gmail.com>
2025-09-20 23:23:43 +0000

    refactor: reorganize VPC and domain configuration in example variables

--- main.tf
+++ main.tf
@@ -16,3 +16,13 @@ module "aws_route53" {
   certificate_arn           = module.aws_acm[0].certificate_arn
   domain_validation_options = module.aws_acm[0].domain_validation_options
 }
+
+module "aws_vpc" {
+  source               = "./modules/aws/vpc"
+  count                = var.deploy_aws ? 1 : 0
+  providers            = { aws = aws.primary }
+  vpc_cidr             = var.aws_vpc_cidr
+  public_subnet_cidrs  = var.aws_public_subnet_cidrs
+  private_subnet_cidrs = var.aws_private_subnet_cidrs
+  tags                 = var.aws_tags
+}
