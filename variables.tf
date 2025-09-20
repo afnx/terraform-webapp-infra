519a510c83070c5b0846ffe77f145c13d11cef18
afnx <afndeveloper@gmail.com>
2025-09-20 22:15:57 +0000

    refactor: rename variables for consistency and clarity in AWS configuration

--- variables.tf
+++ variables.tf
@@ -10,17 +10,7 @@ variable "aws_region" {
   default     = "us-west-2"
 }
 
-variable "domain_name" {
-  type        = string
-  description = "The primary domain name for the ACM certificate."
-}
-
-variable "subject_alternative_names" {
-  type        = list(string)
-  description = "A list of additional domain names for the ACM certificate."
-}
-
-variable "tags" {
+variable "aws_tags" {
   description = "Tags to apply to resources."
   type        = map(string)
   default = {
@@ -28,4 +18,32 @@ variable "tags" {
     Owner       = "your-team"
     Project     = "webapp"
   }
+}
+
+variable "aws_domain_name" {
+  type        = string
+  description = "The primary domain name for the ACM certificate."
+}
+
+variable "aws_subject_alternative_names" {
+  type        = list(string)
+  description = "A list of additional domain names for the ACM certificate."
+}
+
+variable "aws_vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "aws_public_subnet_cidrs" {
+  type        = list(string)
+  description = "List of public subnet CIDRs"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "aws_private_subnet_cidrs" {
+  type        = list(string)
+  description = "List of private subnet CIDRs"
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
 }
