3f5cbbf1a3c4dcba6c2f7c09953129ca9249f681
afnx <afndeveloper@gmail.com>
2025-09-20 23:34:52 +0000

    feat: add KMS key for VPC Flow Logs encryption and configure CloudWatch log group

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -78,6 +78,12 @@ resource "aws_route_table_association" "private" {
 resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
   name              = "/aws/vpc/${aws_vpc.main.id}/flow-logs"
   retention_in_days = 30
+  kms_key_id        = aws_kms_key.vpc_flow_logs.arn
+}
+
+resource "aws_kms_key" "vpc_flow_logs" {
+  description         = "KMS key for VPC Flow Logs encryption"
+  enable_key_rotation = true
 }
 
 resource "aws_flow_log" "vpc" {
