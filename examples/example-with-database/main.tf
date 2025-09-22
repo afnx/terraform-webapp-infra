9a404760d08509b130411b1992698a438ce17f3c
afnx <afndeveloper@gmail.com>
2025-09-22 21:53:44 +0000

    fix: ensure consistent formatting of deploy_aws variable in example configurations

--- examples/example-with-database/main.tf
+++ examples/example-with-database/main.tf
@@ -5,6 +5,8 @@ provider "aws" {
 module "webapp_infra" {
   source = "../../"
 
+  deploy_aws = true
+
   aws_region      = var.aws_region
   aws_domain_name = var.aws_domain_name
   aws_containers  = var.aws_containers
