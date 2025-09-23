3929004de69a822fe6aed8cc23fd0f5ffbe471b6
afnx <afndeveloper@gmail.com>
2025-09-23 22:38:25 +0000

    fix: enhance ECS task definition with health check parameters and optional logging configuration

--- modules/aws/ecs_fargate/variables.tf
+++ modules/aws/ecs_fargate/variables.tf
@@ -1,3 +1,8 @@
+variable "region" {
+  type        = string
+  description = "AWS region to deploy resources in"
+}
+
 variable "vpc_id" {
   type        = string
   description = "VPC ID for ECS tasks"
@@ -62,19 +67,29 @@ variable "service_connect_namespace" {
   description = "Cloud Map namespace for ECS Service Connect"
 }
 
+variable "log_group_name" {
+  type        = string
+  description = "Name of the CloudWatch log group for ECS tasks"
+}
+
 variable "containers" {
   type = map(object({
-    image         = string
-    cpu           = number
-    memory        = number
-    port          = number
-    health_check  = optional(string)
-    public        = bool
-    domain        = optional(string)
-    protocol      = string
-    port_name     = optional(string)
-    desired_count = optional(number)
-    environment   = optional(map(string))
+    image                     = string
+    cpu                       = number
+    memory                    = number
+    port                      = number
+    health_check              = optional(string)
+    health_check_interval     = optional(number)
+    health_check_timeout      = optional(number)
+    health_check_retries      = optional(number)
+    health_check_start_period = optional(number)
+    public                    = bool
+    domain                    = optional(string)
+    protocol                  = string
+    port_name                 = optional(string)
+    desired_count             = optional(number)
+    environment               = optional(map(string))
+    enable_logs               = optional(bool, true)
     secrets = optional(list(object({
       name      = string
       valueFrom = string
